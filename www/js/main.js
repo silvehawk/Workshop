7bf791a7902e27aec4a7fe4a4ec391fe4effa1e3
silvehawk <silverhawk3@web.de>
2014-02-18 15:28:14 +0100

    tut part 3

--- www/js/main.js
+++ www/js/main.js
@@ -1,23 +1,33 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
+	findByName : function() {
+		console.log('findByName');
+		this.store.findByName($('.search-key').val(), function(employees) {
+			var l = employees.length;
+			var e;
+			$('.employee-list').empty();
+			for (var i = 0; i < l; i++) {
+				e = employees[i];
+				$('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+			}
+		});
+	},
 
-    initialize: function() {
-        this.store = new WebSqlStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+	showAlert : function(message, title) {
+		if (navigator.notification) {
+			navigator.notification.alert(message, null, title, 'OK');
+		} else {
+			alert( title ? (title + ": " + message) : message);
+		}
+	},
 
+	initialize : function() {
+		var self = this;
+		this.store = new WebSqlStore(function() {
+			self.showAlert('Store initialized', 'info');
+		});
+		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+	}
 };
 
 app.initialize();
